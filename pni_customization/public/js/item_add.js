b994b8a1fa61f6481f56d8fe10c2c31612730230
Jigar Tarpara <jigartarpara68@gmail.com>
2019-11-04 12:28:20 +0530

    Add Varient

--- pni_customization/public/js/item_add.js
+++ pni_customization/public/js/item_add.js
@@ -1,75 +1,99 @@
 frappe.ui.form.on('Sales Order', {
 	refresh: function(frm) {
-		cur_frm.cscript.add_item_dialog()
+		cur_frm.cscript.add_item_dialog()	
 	}
 });
 
 cur_frm.cscript.add_item_dialog = function(frm) {
 	cur_frm.add_custom_button(__("Add Item by Attribute"), function() {
-		frappe.prompt(
-			[
-				{
-					'fieldname': 'item_varient', 
-					'fieldtype': 'Link', 
-					'options': 'Item', 
-					'label': 'Item',
-					get_query: () => {		
-						return {
-							filters: {
-								has_variants: true
-							}
-						};
-					},
-				},
-			],
-			function(values){
-				frappe.call({
-					method: "pni_customization.utils.get_item_data",
-					args: { 
-						item: values.item_varient
-					},
-					callback: (response) => {
-						var data;
-						data = [
-							{
-								'fieldname': 'item', 
-								'fieldtype': 'Link', 
-								'options': 'Item', 
-								'label': 'Item', 
-								'default': response.message.item , 
-								'read_only':1 
-							}
-						]
-						var attr;
-						for( attr in response.message.attribute ){
-							data.push(
-								{
-									'fieldname': response.message.attribute[attr].attribute, 
-									'fieldtype': 'Data', 
-									'label': response.message.attribute[attr].attribute
-								}
-							)
+		prompt_for_item_template(frm, "items", "Item Varient Selection", true, function (values) {
+			get_attribute_values(frm, "items", "Add Attribute Values", values, function(values, item){
+				prompt_attribute_values(frm, "items", "Add Attribute Values", values, item, function(values, item) {
+					frappe.call({
+						method: "pni_customization.utils.get_item",
+						args: { 
+							values: values,
+							item: item
+						},
+						callback: (response) => {
+							show_alert('This Item Found ' + response.message, 5);
+							var d = frappe.model.add_child(cur_frm.doc, "Sales Order Item", "items");
+							d.item_code = response.message;
+							d.qty = 1;
+							frappe.model.set_value(d.doctype, d.idx, "item_code", response.message);
+							refresh_field("items");
 						}
-						frappe.prompt(
-							data,
-							function(values){
-								frappe.call({
-									method: "pni_customization.utils.get_item",
-									args: { 
-										values: values
-									},
-									callback: (response) => {
-									}
-								})
-							},
-							'Item Varient Selection',
-							'Add Item'
-						)
-					}
-				});
+					})
+				})
+			})
+		});
+	}).addClass('btn-primary');
+}
+var prompt_for_item_template = function(frm, table, title, qty_required, callback) {
+	frappe.prompt(
+		[
+			{
+				'fieldname': 'item_varient', 
+				'fieldtype': 'Link', 
+				'options': 'Item', 
+				'label': 'Item',
+				get_query: () => {		
+					return {
+						filters: {
+							has_variants: true
+						}
+					};
+				},
 			},
-			'Item Varient Selection',
-			'Add Item'
-		)
-	}); 
+		],
+		function(values){
+			callback(values);
+		},
+		__(title),
+		__('Add Item')
+	)
+}
+
+var get_attribute_values = function(frm, table, title, Rdata, callback_for_get_prompt) {
+	frappe.call({
+		method: "pni_customization.utils.get_item_data",
+		args: { 
+			item: Rdata.item_varient
+		},
+		callback: (response) => {
+			callback_for_get_prompt(response, Rdata.item_varient);
+		}
+	});
+}
+
+var prompt_attribute_values = function(frm, table, title, response, item, callback) {
+	var data = [];
+	$.each(response.message.attribute_data || [], function(i, row) {
+		if( row.numeric_values == false) {
+			data.push(
+				{
+					'fieldname': i, 
+					'fieldtype': 'Select', 
+					'label': i,
+					'options' : row.values
+				}
+			)
+		} else {
+			data.push(
+				{
+					'fieldname': i, 
+					'fieldtype': 'Float', 
+					'label': i,
+				}
+			)
+		}
+	})
+	frappe.prompt(
+		data,
+		function(values){
+			callback(values, item);
+		},
+		'Item Varient Selection For ' + response.message.item,
+		'Add Item'
+	)	
 }
--- pni_customization/utils.py
+++ pni_customization/utils.py
@@ -1,4 +1,4 @@
-import frappe
+import frappe, json
 
 def get_permission_query_conditions_for_opportunity(user):
     	if "System Manager" in frappe.get_roles(user):
@@ -21,10 +21,37 @@ def get_permission_query_conditions_for_opportunity(user):
 def get_item_data(item):
 	#return item data
 	item_doc = frappe.get_doc("Item", item)
-	return {"message":"hello world", "item":item, "attribute":item_doc.attributes}
+	attribute_data = {}
+	for row in item_doc.attributes:
+		attribute_doc = frappe.get_doc("Item Attribute", row.attribute)
+		
+		values = []
+		for attr_value in attribute_doc.item_attribute_values:
+			values.append(attr_value.attribute_value)
+		
+		if not attribute_doc.numeric_values:
+			attribute_data.update({row.attribute: {"numeric_values": False, "values": values}})
+		else:
+			attribute_data.update({row.attribute: {"numeric_values": True, "values": []}})
+		
+	return { 
+		"item":item, 
+		"attribute_data":attribute_data
+	}
 
 @frappe.whitelist()
 def get_item(values):
-	for value in values:
-		print(value)
-	return "Hello"
+	attributes = json.loads(values)
+	for attribute in attributes:
+		print(attribute)
+		print(attributes[attribute])
+	items = frappe.get_all("Item",{"variant_of":"Test A"})
+	for item in items:
+		is_varient = True
+		item_doc = frappe.get_doc("Item", item)
+		for varient_attribute in item_doc.attributes:
+			if unicode(varient_attribute.attribute_value) != unicode(attributes[varient_attribute.attribute]):
+				is_varient = False
+		if is_varient == True:
+			return item_doc.name
+	return frappe.throw("Item Not Found")
