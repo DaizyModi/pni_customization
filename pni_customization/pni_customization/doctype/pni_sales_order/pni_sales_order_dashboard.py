f524d479a373e58e67a6fca916c7925ca54a60cd
Jigar Tarpara <jigartarpara68@gmail.com>
2019-11-27 15:07:58 +0530

    PNI Sales Order

---
+++ pni_customization/pni_customization/doctype/pni_sales_order/pni_sales_order_dashboard.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		'fieldname': 'pni_sales_order',
+		'non_standard_fieldnames': {
+			
+		},
+		'internal_links': {
+		},
+		'transactions': [
+			{
+				'label': _('Fulfillment'),
+				'items': ['Sales Invoice', 'Delivery Note']
+			}
+		]
+	}
