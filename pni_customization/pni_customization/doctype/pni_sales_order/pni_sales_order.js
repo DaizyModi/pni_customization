77e31e610a2ca7ec570a639d5b2b9a26a4a4a5a7
Jigar Tarpara <jigartarpara68@gmail.com>
2019-12-02 13:35:01 +0530

    Chages

--- pni_customization/pni_customization/doctype/pni_sales_order/pni_sales_order.js
+++ pni_customization/pni_customization/doctype/pni_sales_order/pni_sales_order.js
@@ -3,6 +3,48 @@
 
 frappe.ui.form.on('PNI Sales Order', {
 	// refresh: function(frm) {
-
+	//	datetime.now()
 	// }
+	onload: function(frm) {
+		if(!frm.doc.date){
+			frm.set_value("date", moment(frappe.datetime.now_datetime()));
+		}
+		if(!frm.doc.delivery_date){
+			frm.set_value("delivery_date", moment(frappe.datetime.now_datetime()));
+		}
+		if(!frm.doc.user){
+			frm.set_value("user",frappe.session.user);
+		}
+		cur_frm.set_query("price_list", function() {
+			return { filters: { selling: 1 } };
+		});
+		frappe.db.get_value('Selling Settings', 'Selling Settings', 'selling_price_list',(r) => {
+			frm.set_value("price_list",r.selling_price_list);
+			frm.refresh_field("price_list");
+		} )
+	},
+	user: function(frm) {
+		cur_frm.set_query("sales_person", function() {
+			return {
+				"filters": {
+					"pni_user": frm.doc.user,
+				}
+			};
+		});
+		// if(!frm.doc.sales_person){
+		// 	frappe.call({
+		// 		"method": "frappe.client.get",
+		// 		args: {
+		// 			doctype: "Sales Person",
+		// 			filters: {"pni_user":frm.doc.user}
+		// 		},
+		// 		callback: function (data) {
+		// 			if(data.message.name){
+		// 				frm.set_value("sales_person",data.message.name);
+		// 			}
+		// 		}
+		// 	})
+		// }
+		
+	}
 });
