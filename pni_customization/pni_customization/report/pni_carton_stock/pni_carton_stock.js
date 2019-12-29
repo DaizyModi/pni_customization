42aeef9303d8d61a0e52e609cb84542e52cfad03
Jigar Tarpara <jigartarpara68@gmail.com>
2019-12-30 00:16:50 +0530

    Chages

---
+++ pni_customization/pni_customization/report/pni_carton_stock/pni_carton_stock.js
@@ -0,0 +1,13 @@
+frappe.query_reports["PNI Carton Stock"] = {
+	"filters": [
+		{
+			"fieldname":"status",
+			"label": __("Status"),
+			"fieldtype": "Select",
+			"options": "Pending\nAvailable\nDelivered",
+			"default": "Available",
+			"reqd": 1,
+			"width": "80",
+		},
+	]
+}
