2993a1b3537e845d7a37a5ae16b57a9f53cfc706
Jigar Tarpara <jigartarpara68@gmail.com>
2020-06-17 16:13:33 +0530

    Chages

--- pni_customization/pni_customization/report/pni_workstation_production_report/pni_workstation_production_report.py
+++ pni_customization/pni_customization/report/pni_workstation_production_report/pni_workstation_production_report.py
@@ -45,17 +45,33 @@ def get_data(filters=None):
 		conditions += " and item.brand = '{0}' ".format(filters.brand)
 
 	return frappe.db.sql("""
-		select 
-			packing.workstation, sum(pni_crt.total), sum(pni_crt.total) 
-		from 
-			`tabPNI Carton` as pni_crt,
-			`tabPNI Packing` as packing, 
-			`tabPNI Packing Carton` as pni_crt_tbl
-			
-		where 
-			pni_crt.name = pni_crt_tbl.carton_id and
-			pni_crt_tbl.parent = packing.name	
+		select table1.workstation,table1.total_production,table2.total_scrap
+			from 
+				(select 
+					packing.workstation as workstation, sum(pni_crt.total) as total_production 
+				from 
+					`tabPNI Carton` as pni_crt,
+					`tabPNI Packing` as packing, 
+					`tabPNI Packing Carton` as pni_crt_tbl
+					
+				where 
+					pni_crt.name = pni_crt_tbl.carton_id and
+					pni_crt_tbl.parent = packing.name	
+
+				group by packing.workstation) as table1,
+				(select 
+					stock_entry.pni_reference as workstation,
+					sum(item_table.qty) as total_scrap
+				from 
+					`tabStock Entry Detail` as item_table,
+					`tabStock Entry` as stock_entry
+				
+				where
+					item_table.name = stock_entry.parent and
+					stock_entry.scrap_entry = "1" and
+					stock_entry.pni_reference = "Workstation"
+				
+				group by stock_entry.pni_reference) as table2
+			where table1.workstation = table2.workstation
 			{0}
-    	
-		group by packing.workstation;
     """.format(conditions))
