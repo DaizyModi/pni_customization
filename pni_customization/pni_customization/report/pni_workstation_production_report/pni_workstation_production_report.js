520e3dc74c7867b57c0866b2a362b03f40190884
Jigar Tarpara <jigartarpara68@gmail.com>
2020-06-17 11:57:31 +0530

    PNI Workstation Production Report

---
+++ pni_customization/pni_customization/report/pni_workstation_production_report/pni_workstation_production_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Jigar Tarpara and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["PNI Workstation Production Report"] = {
+	"filters": [
+
+	]
+};
