2abd19a7b1047ee37254cf6d691e488428e3c568
Jigar Tarpara <jigartarpara68@gmail.com>
2020-06-18 16:40:20 +0530

    ChageS

--- pni_customization/hooks.py
+++ pni_customization/hooks.py
@@ -32,8 +32,7 @@ fixtures = [
 					"scan_reel", "justdial_details", "j_leadid", "j_leadtype", "j_email", "j_date","j_category",
 					"j_city","j_area","j_brancharea","j_dncmobile","j_dncphone","j_company", "j_pincode",
 					"j_time","j_branchpin", "j_parentid","pni_sales_order_item","item_filter",
-					"add_pni_bag", "weight_filter", "section_pni_settings", "section_pni_packing_table", "pni_clm_brk", "pni_paper_blank", "pni_reference_type",
-					"pni_reference","scrap_entry"
+					"add_pni_bag", "weight_filter", "section_pni_settings", "section_pni_packing_table", "pni_clm_brk"
 				)
 			]
 		]
