7b57c2d9fa7af74f9a2ab9c5652b0a4e1691165a
Jigar Tarpara <jigartarpara68@gmail.com>
2020-06-26 19:35:35 +0530

    Chages

--- pni_customization/Custom Scriptsss/sales order.js
+++ pni_customization/Custom Scriptsss/sales order.js
@@ -43,31 +43,32 @@ frappe.ui.form.on("Sales Order Item",{
 				}
 			});
 		}
-		d2.base_uom_rate = parseFloat(d2.price_list_rate / d2.conversion_factor	)
+		// if(d2.price_list_rate>0){
+		// 	d2.base_uom_rate = parseFloat(d2.price_list_rate / d2.conversion_factor	)
+		// }
 	},
 	"is_paper_plate": function(frm, cdt, cdn) {
-		debugger;
 		frappe.meta.get_docfield(cdt, 'rate', cdn).read_only=1
 	},
 	"paper_cup": function(frm, cdt, cdn) {
-		debugger;
 		frappe.meta.get_docfield(cdt, 'rate', cdn).read_only=1
 	},
 	"uom": function(frm, cdt,cdn){
 		var d2 = locals[cdt][cdn];
-		d2.base_uom_rate = parseFloat(d2.price_list_rate / d2.conversion_factor	)
+		// if(d2.price_list_rate>0){
+		// 	d2.base_uom_rate = parseFloat(d2.price_list_rate / d2.conversion_factor	)
+		// }
 	},
 	"qty": function(frm, cdt, cdn){
-		var d2 = locals[cdt][cdn];
-		d2.base_uom_rate = parseFloat(d2.price_list_rate / d2.conversion_factor	)
-		frm.refresh_field("items")
 	},
 	"base_uom_rate": function(frm, cdt, cdn){
 		var d2 = locals[cdt][cdn];
 		if(parseFloat(d2.base_uom_rate * d2.conversion_factor) < d2.price_list_rate & d2.price_list_rate){
 			frappe.msgprint("Rate can't be less then "+parseFloat(d2.price_list_rate / d2.conversion_factor	))
-			d2.base_uom_rate = parseFloat(d2.price_list_rate / d2.conversion_factor	)
-			frm.refresh_field("items")
+			if(d2.price_list_rate>0){
+				d2.base_uom_rate = parseFloat(d2.price_list_rate / d2.conversion_factor	)
+				frm.refresh_field("items")
+			}
 			return;
 		}
 		d2.rate = parseFloat(d2.base_uom_rate * d2.conversion_factor)
@@ -79,8 +80,8 @@ frappe.ui.form.on("Sales Order Item",{
 		var d2 = locals[cdt][cdn];
 		if(d2.rate < d2.price_list_rate){
 			frappe.msgprint("Rate can't be less then "+d2.price_list_rate)
-			d2.base_uom_rate = parseFloat(d2.price_list_rate / d2.conversion_factor	)
-			frm.refresh_field("items")
+			// d2.base_uom_rate = parseFloat(d2.price_list_rate / d2.conversion_factor	)
+			// frm.refresh_field("items")
 		}
 	}
 });
