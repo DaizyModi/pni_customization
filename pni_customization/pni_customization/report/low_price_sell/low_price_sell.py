68eb8935f08a4ffaa54aa0085dd80811b74d6a98
Jigar Tarpara <jigartarpara68@gmail.com>
2020-07-03 15:53:52 +0530

    Chages

--- pni_customization/pni_customization/report/low_price_sell/low_price_sell.py
+++ pni_customization/pni_customization/report/low_price_sell/low_price_sell.py
@@ -2,8 +2,89 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns, data = [], []
+    data = get_data(filters)
+    columns = get_columns()
+    return columns, data
+
+def get_columns():
+    return  [
+        {
+            "fieldname": "sales_order",
+            "label": _("Sales Order"),
+            "fieldtype": "Link",
+            "options": "Sales Order",
+            "width": 150
+        },
+		{
+            "fieldname": "title",
+            "label": _("Title"),
+            "fieldtype": "Data",
+            "width": 150
+        },
+		{
+            "fieldname": "customer",
+            "label": _("Customer"),
+            "fieldtype": "Link",
+			"options": "Customer"
+        },
+		{
+            "fieldname": "date",
+            "label": _("Date"),
+            "fieldtype": "Date",
+        },
+        {
+            "fieldname": "item",
+            "label": _("Item"),
+            "fieldtype": "Link",
+			"options": "Item"
+        },
+        {
+            "fieldname": "price_list_rate",
+            "label": _("Price List Rate"),
+            "fieldtype": "Float",
+        },
+        {
+            "fieldname": "sell_rate",
+            "label": _("Sell Rate"),
+            "fieldtype": "Float",
+        },
+        {
+            "fieldname": "diff",
+            "label": _("Difference"),
+            "fieldtype": "Float",
+        },
+		{
+            "fieldname": "diff_in_per",
+            "label": _("Difference in Percentage"),
+            "fieldtype": "Float",
+        }
+    ]
+
+def get_data(filters=None):
+	conditions = ""
+
+	if filters.from_date:
+		conditions += " and so.creation >= '{0}' ".format(filters.from_date)
+	
+	if filters.to_date:
+		conditions += " and so.creation <='{0}' ".format(filters.to_date)
+
+	return frappe.db.sql("""
+		select 
+			so.name, so.title, so.customer, so.transaction_date, soi.item_code,
+			soi.price_list_rate, soi.rate, 	(soi.price_list_rate - soi.rate) as diff, 
+			((soi.price_list_rate - soi.rate)*100/soi.price_list_rate) as diff_per
+		from 
+			 `tabSales Order` as so, `tabSales Order Item` as soi
+
+		where 
+			so.name = soi.parent and
+			so.docstatus <> "2" and
+			soi.price_list_rate > soi.rate
+			{0};
+    """.format(conditions))
