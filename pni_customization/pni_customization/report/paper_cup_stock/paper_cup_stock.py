7aea9034c130434d8f53826af4af702c1430768a
Jigar Tarpara <jigartarpara68@gmail.com>
2020-07-09 15:54:04 +0530

    Qiuck FIx

--- pni_customization/pni_customization/report/paper_cup_stock/paper_cup_stock.py
+++ pni_customization/pni_customization/report/paper_cup_stock/paper_cup_stock.py
@@ -90,13 +90,13 @@ def get_data(filters=None):
 
 	return frappe.db.sql("""
 		select 
-			crt.item, crt.brand, crt.status, crt.size, crt.no_of_stack, count(crt.item), sum(crt.total), sum(crt.net_weight),
-			sum(crt.gross_weight), bin.actual_qty
+			crt.item, crt.brand, crt.status, crt.size, crt.no_of_stack, crt.nos, crt.total, crt.net_weight,
+			crt.gross_weight, bin.actual_qty
 		
 		from 
 			(select 
-				crt.item, item.brand, crt.status, crt.size, crt.no_of_stack, count(crt.item), sum(crt.total), sum(crt.net_weight),
-				sum(crt.gross_weight)
+				crt.item, item.brand, crt.status, crt.size, crt.no_of_stack, count(crt.item) as nos, sum(crt.total) as total, sum(crt.net_weight) as net_weight,
+				sum(crt.gross_weight) as gross_weight
 			
 			from 
 				`tabItem` as item, `tabPNI Carton` as crt
