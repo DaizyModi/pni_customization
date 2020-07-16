ca75068267bd2225d244f024f02de188dd32660e
Jigar Tarpara <jigartarpara68@gmail.com>
2020-07-16 16:37:29 +0530

    Chages

--- pni_customization/hooks.py
+++ pni_customization/hooks.py
@@ -33,7 +33,7 @@ fixtures = [
 					"j_city","j_area","j_brancharea","j_dncmobile","j_dncphone","j_company", "j_pincode",
 					"j_time","j_branchpin", "j_parentid","pni_sales_order_item","item_filter",
 					"add_pni_bag", "weight_filter", "section_pni_settings", "section_pni_packing_table", "pni_clm_brk","is_paper_cup","base_uom_rate",
-					"approve_law_rate__","need_approval","tear_weight"
+					"approve_law_rate__","need_approval","tear_weight", "unit_price_pni"
 				)
 			]
 		]
