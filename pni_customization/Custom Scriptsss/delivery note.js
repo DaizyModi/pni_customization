2beec974fc338185f093f38a6b83f5058ee67537
Jigar Tarpara <jigartarpara68@gmail.com>
2020-07-21 18:10:49 +0530

    Chages

--- pni_customization/Custom Scriptsss/delivery note.js
+++ pni_customization/Custom Scriptsss/delivery note.js
@@ -50,5 +50,45 @@ frappe.ui.form.on('Delivery Note', {
 		if(frm.doc.is_return && frm.doc.__islocal){
 			frm.set_value("naming_series","MAT-CRN-.YYYY.-");
 		}
+		const doc = frm.doc;
+		debugger;
+		if(doc.payment_terms_template && !doc.payment_schedule.length ) {
+			var posting_date = doc.posting_date || doc.transaction_date;
+			frappe.call({
+				method: "erpnext.controllers.accounts_controller.get_payment_terms",
+				args: {
+					terms_template: doc.payment_terms_template,
+					posting_date: posting_date,
+					grand_total: doc.rounded_total || doc.grand_total,
+					bill_date: doc.bill_date
+				},
+				callback: function(r) {
+					if(r.message && !r.exc) {
+						frm.set_value("payment_schedule", r.message);
+					}
+				}
+			})
+		}
+	},
+	payment_terms_template: function(frm) {
+		debugger;
+		const doc = frm.doc;
+		if(doc.payment_terms_template ) {
+			var posting_date = doc.posting_date || doc.transaction_date;
+			frappe.call({
+				method: "erpnext.controllers.accounts_controller.get_payment_terms",
+				args: {
+					terms_template: doc.payment_terms_template,
+					posting_date: posting_date,
+					grand_total: doc.rounded_total || doc.grand_total,
+					bill_date: doc.bill_date
+				},
+				callback: function(r) {
+					if(r.message && !r.exc) {
+						frm.set_value("payment_schedule", r.message);
+					}
+				}
+			})
+		}
 	}
 });
