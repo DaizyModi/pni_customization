a8c90b89b8473dd7c432379ecbe7490b63833e8c
Jigar Tarpara <jigartarpara68@gmail.com>
2020-08-20 18:01:07 +0530

    Chages

--- pni_customization/utils.py
+++ pni_customization/utils.py
@@ -301,7 +301,7 @@ def submit_delivery_item(doc, method):
 		item_in_table.update({item.item_code:(weight + float(item.stock_qty))})
 	for data in items_calc:
 		if item_in_table.get(data) and items_calc.get(data):
-			if item_in_table.get(data) != items_calc.get(data):
+			if round(item_in_table.get(data),2) != round(items_calc.get(data),2):
 				frappe.throw("{0}'s qty({1}) is not metch in item table qty({2})".format(data,item_in_table.get(row.item), items_calc.get(row.item)))
 def get_carton_warehouse(packing_item, items):
 	for item in items:
