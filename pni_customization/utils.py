c5c2d601a18924b3b6443ed723cfa2f367204b32
Jigar Tarpara <jigartarpara68@gmail.com>
2020-09-14 10:58:20 +0530

    Quick FIx

--- pni_customization/utils.py
+++ pni_customization/utils.py
@@ -576,13 +576,15 @@ def manage_se_cancel(se, co):
 	co.save()
 
 def validate_po(doc, method):
+	item_array_mr,item_array_po = {},{}
 	for item in doc.items:
 		if item.material_request:
 			mr = frappe.get_doc("Material Request", item.material_request)
 			for mr_item in mr.items:
 				if mr_item.item_code == item.item_code:
 					if item.qty > mr_item.qty:
-						frappe.throw("Purchase Order Qty should not greater then Material Request")
+						pass
+						# frappe.throw("Purchase Order Qty should not greater then Material Request")
 
 @frappe.whitelist()
 def manage_se_changes(doc, method):
