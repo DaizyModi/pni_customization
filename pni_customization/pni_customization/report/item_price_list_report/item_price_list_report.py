94f72f8978aff57bcf924172fb4d71ab62d8eb1d
Jigar Tarpara <jigartarpara68@gmail.com>
2020-10-15 17:14:04 +0530

    Chages in Item Price List Report

--- pni_customization/pni_customization/report/item_price_list_report/item_price_list_report.py
+++ pni_customization/pni_customization/report/item_price_list_report/item_price_list_report.py
@@ -26,30 +26,45 @@ def get_columns():
             "fieldtype": "Data",
             "width": 150,
 			"precision":4
+        },
+		{
+            "fieldname": "packing_rate",
+            "label": _("Packing Rate"),
+            "fieldtype": "Data",
+            "width": 150,
+			"precision":4
+        },
+		{
+            "fieldname": "packing_rate_with_gst",
+            "label": _("Packing Rate with GST"),
+            "fieldtype": "Data",
+            "width": 150,
+			"precision":4
         }
+
     ]
 
 def get_data(filters=None):
 	conditions = ""
-	join = ""
 	if filters.brand_group:
-		conditions += " and bgt.brand = ip.brand "
-		join += " ,`tabBrand Group Table` as bgt"
 		conditions += " and bgt.parent = '{0}' ".format(filters.brand_group)
 		
 	conditions += get_condition()
 
 	return frappe.db.sql("""
 		select 
-			ip.brand,ip.price_list_rate
+			ip.brand, ip.price_list_rate, (ip.price_list_rate * bg.multiplier), (ip.price_list_rate * bg.multiplier * 1.18)
 		from 
-			`tabItem Price` as ip
-			{1}
+			`tabItem Price` as ip,
+			`tabBrand Group Table` as bgt,
+			`tabBrand Group` as bg
 		where 
 			ip.customer is NULL
+			and bgt.brand = ip.brand 
+			and bg.name = bgt.parent
 			{0}
 		group by ip.brand
-    """.format(conditions, join))
+    """.format(conditions))
 
 def get_condition():
 	data = frappe.db.get_all("Brand Group", fields=['query', 'role'])
