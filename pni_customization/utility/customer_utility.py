06a54c5f129d85b587f38177004b28e0c42eb3b0
Jigar Tarpara <jigartarpara68@gmail.com>
2020-10-21 17:05:06 +0530

    Chaegs

--- pni_customization/utility/customer_utility.py
+++ pni_customization/utility/customer_utility.py
@@ -1,5 +1,6 @@
 
 def validate_customer(doc, method):
+	return
 	for user in doc.daily_payment_report:
 		doc.last_commitment_date = user.commitment_date
 		doc.last_commitment_amt = user.commitment_amt
