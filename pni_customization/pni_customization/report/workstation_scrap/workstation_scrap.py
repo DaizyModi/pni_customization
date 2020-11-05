1d3c56728ba6d6c011caa4b57e569d375316fb86
Jigar Tarpara <jigartarpara68@gmail.com>
2020-11-05 12:37:32 +0530

    Chages

--- pni_customization/pni_customization/report/workstation_scrap/workstation_scrap.py
+++ pni_customization/pni_customization/report/workstation_scrap/workstation_scrap.py
@@ -149,8 +149,8 @@ def get_data(filters=None):
 				production_data.workstation_head, 
 				production_data.shift, 
 				production_data.production,
-				scrap_data.total_bottom_scrap, 
-				scrap_data.total_blank_scrap
+				"0", 
+				"0"
 			from
 				(
 					select 
