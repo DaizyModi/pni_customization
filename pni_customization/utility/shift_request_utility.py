7f48b6c45f928b21bb0d8d03a80a6e184e10e9da
Jigar Tarpara <jigartarpara68@gmail.com>
2020-11-24 10:59:41 +0530

    Chages

--- pni_customization/utility/shift_request_utility.py
+++ pni_customization/utility/shift_request_utility.py
@@ -2,5 +2,5 @@ import frappe
 from frappe.utils import getdate,today
 
 def validate_shift_request(doc, method):
-	if getdate(doc.from_date) < today():
+	if getdate(doc.from_date) < getdate(today()):
 		frappe.throw("From Date can't be back date")
