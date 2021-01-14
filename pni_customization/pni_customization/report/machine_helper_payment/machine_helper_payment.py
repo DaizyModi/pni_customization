6f068a341b6cc3b35e41120f568d3e3496348fb3
Jigar Tarpara <jigartarpara68@gmail.com>
2021-01-14 18:21:47 +0530

    Chages

---
+++ pni_customization/pni_customization/report/machine_helper_payment/machine_helper_payment.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2013, Jigar Tarpara and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+
+def execute(filters=None):
+    columns, data = [], []
+    data = get_data(filters)
+    columns = get_columns()
+    return columns, data
+
+def get_columns():
+    return  [
+		{
+            "fieldname": "workstation_head",
+            "label": _("Workstation Head"),
+            "fieldtype": "Data",
+            "width": 150
+        },
+        {
+            "fieldname": "machine_helper",
+            "label": _("Machine Helper"),
+            "fieldtype": "Data",
+            "width": 150
+        },
+		{
+            "fieldname": "total_stock",
+            "label": _("Total Stock"),
+            "fieldtype": "Float",
+            "width": 150,
+			"precision":4
+        },
+		{
+            "fieldname": "total_pay",
+            "label": _("Total Pay"),
+            "fieldtype": "Float",
+            "width": 150,
+			"precision":4
+        },
+		{
+            "fieldname": "dumy",
+            "label": _("Empty"),
+            "fieldtype": "Data",
+            "width": 150,
+			"precision":4
+        }
+    ]
+
+def get_data(filters=None):
+	conditions = ""
+	if filters.from_date:
+		conditions += " and date >= '{0}' ".format(filters.from_date)
+	
+	if filters.to_date:
+		conditions += " and date <='{0}' ".format(filters.to_date)
+	
+	if filters.workstation_head:
+		conditions += " and workstation_head like '%{0}%' ".format(filters.workstation_head)
+	
+	return frappe.db.sql("""
+		select 
+			workstation_head,
+			machine_helper,
+			sum(total_stock),
+			sum(total_pay)
+		from
+			`tabPNI Packing`
+		where
+			docstatus = 1 {0}
+		group by 
+			machine_helper;
+    """.format(conditions))
