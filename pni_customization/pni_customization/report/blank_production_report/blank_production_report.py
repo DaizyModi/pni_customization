cb8c64771b6b1aca18cfd423e6c64bceda9a1492
Jigar Tarpara <jigartarpara68@gmail.com>
2021-01-16 19:15:46 +0530

    Chages

--- pni_customization/pni_customization/report/blank_production_report/blank_production_report.py
+++ pni_customization/pni_customization/report/blank_production_report/blank_production_report.py
@@ -31,7 +31,7 @@ def get_columns():
 		{
             "fieldname": "packing_category",
             "label": _("Packing Category"),
-            "fieldtype": "Float",
+            "fieldtype": "Data",
             "width": 150,
 			"precision":4
         },
