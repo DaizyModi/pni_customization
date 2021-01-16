0fd6bcfa52e37af09d7d34ba1dad5b52adb2aa1b
Jigar Tarpara <jigartarpara68@gmail.com>
2021-01-16 11:17:14 +0530

    Quick Fix

--- pni_customization/pni_customization/doctype/packing/packing.py
+++ pni_customization/pni_customization/doctype/packing/packing.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import get_datetime, time_diff_in_hours
+from frappe.utils import get_datetime, time_diff_in_hours, flt
 
 class Packing(Document):
 	def validate(self):
