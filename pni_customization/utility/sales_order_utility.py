9d3dc9d34a592347984e179c5038803300edcae6
Jigar Tarpara <jigartarpara68@gmail.com>
2021-01-22 19:13:30 +0530

    Chages

--- pni_customization/utility/sales_order_utility.py
+++ pni_customization/utility/sales_order_utility.py
@@ -1,7 +1,15 @@
 import frappe
 from frappe.utils import today
+from erpnext.selling.doctype.customer.customer import get_customer_outstanding, get_credit_limit
 
 def sales_order_before_submit(so,method):
 	so.transaction_date = today()
 	if so.delivery_date < today():
 		so.delivery_date = today()
+
+@frappe.whitelist()
+def get_credit_details(customer, company):
+	outstanding = get_customer_outstanding(customer, company)
+	credit_limit = get_credit_limit(customer, company)
+	bal = credit_limit - outstanding
+	return {"outstanding":outstanding, "credit_limit":credit_limit, "bal": bal}
