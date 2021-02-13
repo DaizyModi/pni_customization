4bebe1a309cf95d1439a93493324588b6c03181b
Jigar Tarpara <jigartarpara68@gmail.com>
2021-02-13 12:08:21 +0530

    Chages

--- pni_customization/utility/installation_note.py
+++ pni_customization/utility/installation_note.py
@@ -1,6 +1,7 @@
 import frappe
 
 
+@frappe.whitelist()
 def get_pre_installation_check_item(pre_installation_check):
     doc = frappe.get_doc("Pre Installation Check", pre_installation_check)
     if doc:
