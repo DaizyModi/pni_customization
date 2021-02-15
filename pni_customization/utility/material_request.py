bb31d8f94a1a511709f327c1916bbc999f4e37b9
Jigar Tarpara <jigartarpara68@gmail.com>
2021-02-15 12:53:09 +0530

    Chages

--- pni_customization/utility/material_request.py
+++ pni_customization/utility/material_request.py
@@ -3,8 +3,8 @@ import frappe
 
 def validate(mr, method):
     for raw in mr.items:
-        if not check_item_workflow_state(raw.item):
-            frappe.throw("{0} Invalid Item ".format(raw.item))
+        if not check_item_workflow_state(raw.item_code):
+            frappe.throw("{0} Invalid Item ".format(raw.item_code))
 
 
 def check_item_workflow_state(item):
