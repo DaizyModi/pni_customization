77b3b2dd95a9e9189e13885843ed87aa4c004fa0
Jigar Tarpara <jigartarpara68@gmail.com>
2021-02-17 11:12:28 +0530

    Quick Fix

--- pni_customization/utility/customer_complaint_form_utility.py
+++ pni_customization/utility/customer_complaint_form_utility.py
@@ -1,7 +1,8 @@
 import frappe
 
+
 @frappe.whitelist()
-def get_items(sales_invoice):
-    doc = frappe.get_doc('Sales Invoice', sales_invoice)
+def get_items(filters):
+    doc = frappe.get_doc('Sales Invoice', filters.get("sales_invoice"))
     for item in doc.get('items'):
         return item.item_code
