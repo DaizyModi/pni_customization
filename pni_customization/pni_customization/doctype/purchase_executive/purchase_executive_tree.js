7f61ff364cce5b7bebe79695fb9a5341790a13af
Daizy Modi <modidaizy6730@gmail.com>
2021-02-19 13:13:01 +0530

    [Purchase Executive] - * Added Doctype * Link field with PO, PR ad PI * Dashboard on doctype * Treeview

---
+++ pni_customization/pni_customization/doctype/purchase_executive/purchase_executive_tree.js
@@ -0,0 +1,23 @@
+frappe.treeview_settings['Purchase Executive'] = {
+    fields: [
+        {
+            fieldtype: 'Data',
+            fieldname: 'purchase_executive_name',
+            label: __('New Purchase Executive'),
+            reqd: true
+        },
+        {
+            fieldtype: 'Link',
+            fieldname: 'employee',
+            label: __('Employee'),
+            options: 'Employee',
+            description: __("Please enter Employee Id of this Purchase Executive")
+        },
+        {
+            fieldtype: 'Check',
+            fieldname: 'is_group',
+            label: __('Group Node'),
+            description: __("Further Executives can be only created under 'Group' type nodes")
+        }
+    ],
+}
