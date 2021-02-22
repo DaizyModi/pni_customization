df8b62d74b055e26c72cba8474693e14ce35d004
Daizy Modi <modidaizy6730@gmail.com>
2021-02-22 11:49:40 +0530

    [PNI Gate Entry] - * validation fix for entry status * field for purchase order * fetch items from po in child table

--- pni_customization/pni_customization/doctype/pni_gate_entry/pni_gate_entry.py
+++ pni_customization/pni_customization/doctype/pni_gate_entry/pni_gate_entry.py
@@ -7,18 +7,33 @@ import frappe
 from frappe.model.document import Document
 from frappe.utils import get_datetime
 
+
 class PNIGateEntry(Document):
-	def validate(self):
-		if self.gate_entry_type == "Document Receive":
-			self.entry_status = "Pending For Delivery"
-			if not self.employee:
-				frappe.throw("Employee is Mandatory")
-		elif self.gate_entry_type == "Material Receipt":
-			self.entry_status = "Pending For Delivery"
-		else:
-			self.entry_status = ""
-		if (not self.sender_name1) and (self.gate_entry_type != "Visitor") :
-			frappe.throw("Sender Name is Mandatory")
-		
-		if self.gate_entry_type == "Visitor" and get_datetime(self.in_time) > get_datetime(self.out_time):
-			frappe.throw("In time must be less than to out time")
+    def validate(self):
+        if self.gate_entry_type == "Document Receive":
+            self.entry_status = "Pending For Delivery"
+            if not self.employee:
+                frappe.throw("Employee is Mandatory")
+        elif self.gate_entry_type == "Material Receive":
+            self.entry_status = "Pending For Delivery"
+        else:
+            self.entry_status = ""
+        if (not self.sender_name1) and (self.gate_entry_type != "Visitor"):
+            frappe.throw("Sender Name is Mandatory")
+
+        if self.gate_entry_type == "Visitor" and get_datetime(self.in_time) > get_datetime(self.out_time):
+            frappe.throw("In time must be less than to out time")
+
+
+@frappe.whitelist()
+def get_po_items(po):
+    po_doc = frappe.get_doc('Purchase Order', po)
+    po_items = []
+    for item in po_doc.get('items'):
+        po_items.append({
+            'item_code': item.item_code,
+            'item_name': item.item_name,
+            'item_qty': item.qty,
+            'item_uom': item.stock_uom
+        })
+    return po_items
