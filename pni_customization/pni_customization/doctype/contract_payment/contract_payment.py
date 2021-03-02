ed8d2962535aa3d37714c4e9a5370bbefd4b4daf
Daizy Modi <modidaizy6730@gmail.com>
2021-03-02 13:48:24 +0530

    [Contract Payment ] - * calculate 60% salary of 20 days * validation * payment request date validation

--- pni_customization/pni_customization/doctype/contract_payment/contract_payment.py
+++ pni_customization/pni_customization/doctype/contract_payment/contract_payment.py
@@ -5,30 +5,43 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import datetime
 
 
 class ContractPayment(Document):
     def validate(self):
         if self.advance_payment_request:
-            advance_request = frappe.get_all("Contract Payment", {
-                'advance_payment_request': self.advance_payment_request})
+            advance_request = frappe.get_all("Contract Payment", filters={
+                'advance_payment_request': self.advance_payment_request, 'name': ["!=", self.name], 'month': self.month, 'year': self.year})
+            print(advance_request)
             if advance_request:
                 frappe.throw("A request already submitted once")
 
+        if self.month and self.year:
+            month = datetime.datetime.strptime(self.month, "%B")
+            year = datetime.datetime.strptime(self.year, "%Y")
+            first_day = frappe.utils.data.get_first_day(self.month)
+            last_day = frappe.utils.data.get_last_day(self.month)
+            self.from_date = str(first_day)
+            self.to_date = str(last_day)
+
         if self.person_name:
-            outstanding = frappe.db.sql(""" 
+            outstanding = frappe.db.sql("""
                     select sum(cpr.paid_amount)
                         from
                             `tabContract Payment` as cpr
                         where
                             cpr.docstatus = 1
                             and cpr.person_name = '{emp}'
+                            and cpr.from_date >= '{start_date}'
+                            and cpr.to_date <= '{end_date}'
                         group by
                             cpr.person_name
-                """.format(emp=self.person_name))
+                """.format(emp=self.person_name, start_date=self.from_date, end_date=self.to_date))
 
             if self.person_type == "Worker":
-                total_amount = frappe.db.sql(""" 
+                month = self.month
+                total_amount = frappe.db.sql("""
                         select sum(pt.paying_amount)
                             from
                                 `tabPacking Table` as pt, `tabPacking` as packing
@@ -36,14 +49,41 @@ class ContractPayment(Document):
                                 packing.docstatus = 1
                                 and pt.parent = packing.name
                                 and pt.employee = '{emp}'
-                            group by 
+                                and packing.date >= '{start_date}'
+                                and packing.date <= '{end_date}'
+                            group by
                                 pt.employee,pt.item;
-                    """.format(emp=self.person_name))
+                    """.format(emp=self.person_name, start_date=self.from_date, end_date=self.to_date))
 
-            if total_amount and not outstanding:
-                self.outstanding_amount = total_amount[0][0]
-            elif total_amount and outstanding:
-                self.outstanding_amount = total_amount[0][0] - \
-                    outstanding[0][0]
-            else:
-                return 0
+                if total_amount and not outstanding:
+                    self.outstanding_amount = total_amount[0][0]
+                elif total_amount and outstanding:
+                    self.outstanding_amount = total_amount[0][0] - \
+                        outstanding[0][0]
+                else:
+                    return 0
+
+    def on_submit(self):
+        if self.payment_required_by:
+            first_day_of_reqd_date = frappe.utils.data.get_first_day(
+                self.payment_required_by)
+            last_day_of_reqd_date = frappe.utils.data.get_last_day(
+                self.payment_required_by)
+            month = first_day_of_reqd_date.month
+            year = first_day_of_reqd_date.year
+            middle_date_obj = datetime.date(year, month, 20)
+            middle_date = str(middle_date_obj)
+
+            if self.payment_required_by <= middle_date:
+                frappe.throw(
+                    "Payment can't done before {0}".format(middle_date))
+
+            if self.paid_amount > 0:
+                if self.paid_amount > self.calculate_paid_amount():
+                    frappe.throw("Paid amount can't be more than {0}".format(
+                        self.calculate_paid_amount()))
+
+    def calculate_paid_amount(self):
+        per_day_amount = (self.outstanding_amount / 30) * 20
+        paid_amount = per_day_amount * 0.6
+        return paid_amount
